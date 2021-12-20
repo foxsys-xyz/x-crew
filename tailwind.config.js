018b99a153392626e0c60c6cdfecc13988723669
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2021-12-20 14:58:20 +0530

    changes to build & packages 👌

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,24 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  // mode: 'jit',
-  purge: [
-    './storage/framework/views/*.php',
-    './resources/**/*.blade.php',
-    './resources/**/*.js',
-    './resources/**/*.vue',
-  ],
-  theme: {
-    extend: {
-      fontSize: {
-        sm: ['0.775rem', '1.15rem'],
-      },
-      colors: {
-        gray: colors.gray,
-      },
-    },
-  },
-  variants: {},
-  plugins: [
-    require('@tailwindcss/forms')
-  ],
+	// mode: 'jit',
+	content: [
+		'./storage/framework/views/*.php',
+		'./resources/**/*.blade.php',
+		'./resources/**/*.js',
+		'./resources/**/*.vue',
+	],
+	theme: {
+		extend: {
+			fontSize: {
+				sm: ['0.775rem', '1.15rem'],
+			},
+			colors: {
+				gray: colors.zinc,
+			},
+		},
+	},
+	plugins: [
+		require('@tailwindcss/forms')
+	],
 }
