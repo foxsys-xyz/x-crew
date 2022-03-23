9fbe5e31a85bda591ae0a6cd34517f3914e5c945
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2022-03-23 10:58:07 +0530

    fix build checks 👐🏻

--- app/Http/Controllers/ACARS/LoginController.php
+++ app/Http/Controllers/ACARS/LoginController.php
@@ -29,7 +29,7 @@ class LoginController extends Controller
 
         $user = User::where('username', request('username'))->first();
 
-        if (!$user || !Hash::check(request('password'), $user->password)) {
+        if (! $user || ! Hash::check(request('password'), $user->password)) {
             return response([
                 'message' => 'Invalid Credentials.'
             ], 401);
